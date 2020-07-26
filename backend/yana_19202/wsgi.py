85fcadd3ac2b8c13182c05cceb89cb675c210ab8
Crowdbotics <team@crowdbotics.com>
2020-07-26 22:46:12 +0000

    Update models

--- backend/yana_19202/wsgi.py
+++ backend/yana_19202/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yana_19202.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yana_19202.settings")
 
 application = get_wsgi_application()
